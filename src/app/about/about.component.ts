44c6a0b27cd5c4c9cab6d2165604cfd01eff385c
Daniel Corner <dcorn068@gmail.com>
2018-07-23 18:32:37 -0400

    initialize navbar dropdown & links

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,17 +3,138 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   template: `
-    <p>
-      about works!
+  <div class="main-grid container-grid">
+    <div class="responsive-picture picture-1">
+      <img src="../../assets/Adrianne2018.jpeg">
+    </div>
+    <div class="container-grid container-1">
+      <p class="paragraph paragraph-1">
+      Adrianne is a user experience designer and researcher in Canada.
+      Her most recent works have focused on improving the value of Government of Canada’s
+      digital products.<br><br>
+      She is flexible and proficient with her tools;
+      her favourites are online and collaborative (E.g. G Suite, Lucidchart),
+      reliable and comprehensive (E.g. Adobe’s Creative Suite), and easy to map
+      information on (E.g. Excel). She holds a Bachelor of Design, Industrial Design,
+      from OCAD University. For her full résumé, please see here.<br><br>
+      </p>
+    </div>
+    <p class="paragraph paragraph-2">
+				<br><a class="profile-link" href="pdf/a-h-l_resume_2018.pdf" target="_blank" title="Résumé">Résumé</a>
+				<br><a class="profile-link" href="https://www.linkedin.com/in/adriannehlee/" target="_blank" title="Linkedin">Professional face</a>
+				<br><a class="profile-link" href="https://twitter.com/adriannehlee" target="_blank" title="Twitter Profile">Casual 280 characters</a>
     </p>
+  </div>
   `,
-  styles: []
+  styles: [
+    `
+      /* Text Elements */
+
+      p.paragraph.paragraph-1 {
+        margin-top: 10px;
+        max-width: 80vmin;
+
+        grid-area: 3/2 / auto / 4;
+      }
+
+      p.paragraph.paragraph-2 {
+        grid-area: 4/2 / auto / auto;
+      }
+
+      .container-grid.main-grid {
+        display: grid;
+        padding-right: 0;
+        padding-left: 0;
+
+        grid-template-columns: 9.8vw auto 41vw;
+        grid-template-rows: 33.2% auto;
+      }
+
+      .container-grid {
+        margin-right: 0;
+        margin-left: 0;
+      }
+
+      div.responsive-picture.picture-1 {
+        margin-top: 35px;
+        margin-bottom: 20px;
+        max-width: none;
+        width: 300px;
+        height: 286px;
+
+        grid-area: 2/2 / auto / auto;
+        -webkit-align-self: flex-end;
+        -ms-flex-item-align: end;
+        align-self: flex-end;
+        justify-self: auto;
+        position: relative;
+      }
+
+      div.responsive-picture.picture-1 > img {
+        width: 46.2%;
+        position: absolute;
+        bottom: 0;
+        border: 5px solid black;
+      }
+
+      .container-grid.container-1 {
+        grid-area: 3/2 / auto / 4;
+      }
+
+      @media screen and (min-width: 62rem) {
+        /* Text Elements */
+
+        p.paragraph.paragraph-1 {
+          grid-area: 1/2 / auto / 2;
+        }
+
+        p.paragraph.paragraph-2 {
+          margin-top: 50px;
+          margin-right: 0;
+          margin-left: 5px;
+
+          grid-area: 2/2 / auto / auto;
+        }
+
+        div.responsive-picture.picture-1 {
+          margin-left: 5px;
+          pointer-events: none;
+          -webkit-align-self: flex-end;
+          -ms-flex-item-align: end;
+          align-self: flex-end;
+          grid-area: 1/2 / auto / auto;
+        }
+
+        .container-grid.main-grid {
+          grid-template-columns: auto 41vw;
+          grid-template-rows: 33.2vw auto;
+        }
+
+        .container-grid.container-1 {
+          display: grid;
+
+          grid-area: 2/1 / auto / auto;
+          grid-template-columns: 0.45fr 0.55fr;
+          grid-template-rows: auto;
+        }
+      }
+
+      @media screen and (min-width: 75rem) {
+        .container-grid.main-grid {
+          display: grid;
+
+          grid-template-columns: auto 41vw;
+          grid-template-rows: 33.2vw auto;
+        }
+      }
+
+      @media screen and (min-width: 93.75rem) {
+      }
+    `
+  ]
 })
 export class AboutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatMenuTrigger } from '@angular/material';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-navbar',
@@ -13,14 +14,22 @@ import { MatMenuTrigger } from '@angular/material';
 
     <span class="grid-item"></span>
 
-    <div class="grid-item right" [matMenuTriggerFor]="menu" >
-      <button mat-button class="right-align">About</button>
-      <button mat-button (mouseenter)="open()">Work<mat-icon>arrow_drop_down</mat-icon></button>
-      <mat-menu #menu="matMenu" xPosition="before" [overlapTrigger]="false">
-        <button mat-menu-item>Services Catalogue</button>
-        <button mat-menu-item>Financial Products Selector</button>
-        <button mat-menu-item>The Store</button>
-      </mat-menu>
+    <div class="grid-item right">
+
+      <a class="nav-link nav-link-full" routerLink="/about">
+			About</a>
+      <div class="dropdown">
+        <a class="nav-link nav-link-full" routerLink="/work">
+        Work&nbsp;<mat-icon>arrow_drop_down</mat-icon>
+        </a>
+        <div id="work-dropdown-full">
+          <a *ngFor="let project of projects"
+          class="dropdown-content-large"
+          routerLink="/{{project.route}}">
+          {{project.title}}</a>
+        </div>
+      </div>
+
     </div>
 
     <span class="grid-item"></span>
@@ -47,21 +56,118 @@ import { MatMenuTrigger } from '@angular/material';
       .grid-container {
         display: grid;
         grid-template-columns: 10% auto 40% 170px 10%;
+        position: relative;
       }
       .grid-item {
         padding: 14px 0;
       }
       .mat-menu-panel {
       }
+      button {
+        cursor: pointer;
+      }
+      #work-dropdown-full {
+        z-index: 9;
+        position: absolute;
+        right: 10%;
+        top: 64px;
+        height: 195px;
+        pointer-events: none;
+        background: white;
+        width: 240px;
+        height: 300;
+        padding: 20px 20px;
+        border-top: 3px solid black;
+        opacity: 0;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        transition: opacity 0.15s ease-in-out;
+      }
+      .dropdown-item a {
+        padding: 12px 16px;
+        display: block;
+      }
+      a.nav-link {
+        color: #dbdbdb;
+        transition: color 0.4s ease-in-out;
+        display: block;
+        margin-right: 0px;
+        font-weight: 500;
+        text-decoration: underline;
+        padding: 10px 5%;
+        width: 50vw;
+      }
+      a.nav-link-full {
+        text-decoration: none;
+        display: inline;
+        padding: 20px 10px;
+      }
+      a.nav-link:hover {
+        color: #df6124;
+        transition: color 0.4s ease-in-out;
+      }
+      .dropdown {
+        display: inline;
+        padding-bottom: 30px;
+        transition: opacity 0.45s ease-in-out;
+      }
+      .dropdown:hover #work-dropdown-full {
+        pointer-events: auto;
+        opacity: 1;
+      }
+      .dropdown-content-large {
+        color: black;
+        display: block;
+        padding: 6px 20px;
+        text-decoration: none;
+        font-weight: 600;
+        font-size: 14px;
+        line-height: 3em;
+      }
+      .dropdown-content-large:hover {
+        color: black;
+        text-decoration: none;
+        background: #f9f9f9;
+        transition: background 0.4s ease-in-out;
+      }
+      li.work-dropdown-item {
+        list-style-type: none;
+        padding-left: 5px;
+      }
     `
   ]
 })
 export class NavbarComponent implements OnInit {
-  @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
 
-  open() {
-    this.trigger.openMenu();
-  }
+  public projects = [
+    { title: 'Services Catalogue', route: 'services-catalogue' },
+    { title: 'Financial Products Selector', route: 'financial-products-selector' },
+    { title: 'The Store', route: 'the-store' },
+  ];
+  // @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
+
+  // open() {
+  //   this.trigger.openMenu();
+  //   // move the buttons back to the top
+  //   $('.button').css('pointer-events', 'auto');
+  //   // close the menu on mouseout
+  // }
+  // close() {
+  //   this.trigger.closeMenu();
+  // }
+
+  //       <button mat - button
+  // routerLink = "/about"
+  // class="right-align" > About < /button>
+  //   < button mat - button[matMenuTriggerFor]="menu"
+  //     (mouseenter) = "open()"
+  // routerLink = "/work" > Work < mat - icon > arrow_drop_down < /mat-icon></button >
+  //   <mat-menu #menu = "matMenu"
+  // xPosition = "before"[overlapTrigger] = "false"
+  //   >
+  //   <button mat - menu - item > Services Catalogue < /button>
+  //     < button mat - menu - item > Financial Products Selector < /button>
+  //       < button mat - menu - item > The Store < /button>
+  //         < /mat-menu>
   constructor() {}
 
   ngOnInit() {}
